e2e8cddcd68e8d4111eca5f9a20c2b11121cd923
Erik <Mr.erik.egliens@gmail.com>
2022-04-01 21:10:38 +0300

    separated data into a separate file

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import Joke from "./components/Joke/Joke";
+import jokesData from "./components/jokesData";
 
 function App() {
-  return (
-    <div className="jokes-wrap">
-      <Joke
-        setup="I got my daughter a fridge for her birthday."
-        punchline="I can't wait to see her face light up when she opens it."
-        isPun={true}
-      />
-      <Joke
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware!"
-      />
-      <Joke
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy."
-        comments={["Comment 1", "Comment 2"]}
-      />
-      <Joke
-        setup="Why do bees stay in the hive in the winter?"
-        punchline="Swarm."
-        isPun={true}
-        rating={2}
-      />
-      <Joke
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-      />
-    </div>
-  );
+  const jokeElements = jokesData.map((joke) => {
+    return <Joke punchline={joke.punchline} setup={joke.setup} />;
+  });
+  return jokeElements;
 }
 
 export default App;
--- src/components/Joke/Joke.js
+++ src/components/Joke/Joke.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-function Joke({ setup, punchline, rating, comments, isPun }) {
-  const classes = isPun ?  ['joke pun'] : ['joke']
+function Joke({setup, punchline}) {
+  console.log(setup);
   return (
     <>
-      <section className={classes}>
+      <section>
         <div className="container">
           {setup ? <h1>Setup: {setup}</h1> : ""}
           {punchline ? <p>Punchline: {punchline}</p> : ""}
-          {rating ? <span>{rating}</span> : ""}
-          {comments ? <div>{comments}</div> : ""}
         </div>
       </section>
     </>
